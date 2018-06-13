40298add74357e45514e4b6411c8f8daafdfa5ac
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2018-06-12 22:38:25 -0500

    Added file upload functionality, working on delete page.

---
+++ RazorPagesMovie/Pages/Schedules/Delete.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Schedules
+{
+    public class DeleteModel : PageModel
+    {
+
+        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+
+        [BindProperty]
+        public Schedule Schedule { get; set; }
+
+        public DeleteModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id != null) {
+                Schedule = _context.Schedule.Where(s => s.ID == id).SingleOrDefault();
+                return Page();
+            }
+
+            return NotFound();
+
+        }
+    }
+}
--- RazorPagesMovie/Pages/Schedules/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Schedules/Index.cshtml.cs
@@ -12,14 +12,59 @@ namespace RazorPagesMovie.Pages.Schedules
     public class IndexModel : PageModel
     {
 
+        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+
+        public IndexModel (RazorPagesMovie.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public FileUpload FileUpload { get; set; }
-        [BindProperty]
+
         public IList<Schedule> Schedule { get; set; }
 
-
-        public void OnGet()
+        public async Task OnGetAsync ()
         {
+            Schedule = await _context.Schedule.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostAsync ()
+        {
+            //check for initial validation errors
+            if (!ModelState.IsValid)
+            {
+                Schedule = await _context.Schedule.AsNoTracking().ToListAsync();
+                return Page();
+            }
+
+            //check for file validation errors
+            var publicScheduleData =
+                await FileHelper.ProcessFormFile(FileUpload.UploadPublicSchedule, ModelState);
+            var privateScheduleData =
+                await FileHelper.ProcessFormFile(FileUpload.UploadPrivateSchedule, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                Schedule = await _context.Schedule.AsNoTracking().ToListAsync();
+                return Page();
+            }
+
+            //post to database
+            var schedule = new Schedule()
+            {
+                PrivateSchedule = privateScheduleData,
+                PrivateScheduleSize = FileUpload.UploadPrivateSchedule.Length,
+                PublicSchedule = publicScheduleData,
+                PublicScheduleSize = FileUpload.UploadPublicSchedule.Length,
+                Title = FileUpload.Title,
+                UploadDT = DateTime.UtcNow
+            };
+
+            _context.Schedule.Add(schedule);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
 
         }
     }
